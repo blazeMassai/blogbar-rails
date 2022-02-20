325efccb77ef7c42a3221f3a43371a4d2d6b6e68
Reward I Masalla <reward.masalla@trc.co.tz>
2022-02-20 21:00:05 +0300

    first commit

---
+++ app/helpers/application_helper.rb
@@ -0,0 +1,14 @@
+module ApplicationHelper
+
+#  return the full title on a per-page basisi
+
+  def full_title(page_title = '')
+    base_title = "Ruby On Rails"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
+
+end
