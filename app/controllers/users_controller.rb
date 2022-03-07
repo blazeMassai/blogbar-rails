2dcf131653a0b18a578020bfd3e4fe9b125c2394
MR.RIL MASALLA <reward.masalla@trc.co.tz>
2022-03-07 17:15:03 +0300

    table za users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,30 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
   def show
     @user = User.find(params[:id])
-
   end
 
   def index
     @users = User.all
   end
+
+  def create
+    @user = User.new(user_params)
+    # render plain: @user.inspect
+    if @user.save
+      flash[:notice] = "User was created successfully!"
+      redirect_to user_path(@user)
+    else
+      render 'new'
+    end
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+
 end
